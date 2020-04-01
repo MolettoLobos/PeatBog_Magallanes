8032ff8bffd19d2f7778e6a9c353dbc10efc2cb6
MolettoLobos <italo.moletto@agrospace.cl>
2020-04-01 09:39:24 -0300

    fixed minor bugs

--- R/feature_selection_data.R
+++ R/feature_selection_data.R
@@ -10,14 +10,21 @@ shp = readOGR(dsn)
 data = shp@data
 
 correlation=cor(data, use = "complete.obs")
-maitenes_index_filtered_NA = na.omit(data)
-pred = data[,-1]
-var = data[,1]
+data_NA = na.omit(data)
+pred = data_NA[,-1]
+var = data_NA[,1]
 # GET BEST INDEX TO PREDICT BY LOOCV------------------------------------------------
 set.seed(550) 
 control <- rfeControl(functions=rfFuncs, method="LOOCV", verbose = T) 
 rfeDAT<- rfe(pred, var, rfeControl=control); rfeDAT
 print(rfeDAT); pred(rfeDAT)
+# summarize the results
+print(rfeDAT)
+# list the chosen features
+predictors(rfeDAT)
+# plot the results
+plot(rfeDAT, type=c("g", "o"))
+
 
 df<- pickVars(rfeDAT$variables, 4)
 prueba  <- data.frame(maitenes_index_filtered_NA[,c(df)],var)
