b38def9f74ad741a39805a0ca83bf358515b116f
MolettoLobos <italo.moletto@agrospace.cl>
2020-04-03 19:11:26 -0300

    updated spectra plot

--- R/spectral_separability.R
+++ R/spectral_separability.R
@@ -6,6 +6,7 @@ library(spatialEco)
 library(rlist)
 library(corrplot)
 library(ggplot2)
+library(matlab)
 # get data for processing ------------------------------------------------
 path = 'C:/Users/italo/Google Drive/PeatBog_Magallanes'
 dsn = paste0(path,'/','lc_data.shp')
@@ -55,8 +56,11 @@ master_df = data.frame(S2Bands=S2Bands,
                        ext_ForestC2019_mean=t(ext_ForestC2019_statistics[,1:12]),
                        ext_Herb2019_mean=t(ext_Herb2019_statistics[,1:12]),
                        ext_HerbsB2019_mean=t(ext_HerbsB2019_statistics[,1:12]),
+                       ext_Ice2019_mean=t(ext_Ice2019_statistics[,1:12]),
                        ext_PeatA2019_mean=t(ext_PeatA2019_statistics[,1:12]),
                        ext_PeatB2019_mean=t(ext_PeatB2019_statistics[,1:12]),
+                       ext_BushC2019_mean=t(ext_BushC2019_statistics[,1:12]),
+                       ext_Rocks2019_mean=t(ext_Rocks2019_statistics[,1:12]),
                        ext_Sombra2019_mean=t(ext_Sombra2019_statistics[,1:12]),
                        ext_Waterbodies2019_mean=t(ext_Waterbodies2019_statistics[,1:12]),
                        ext_BushA2019_sd=t(ext_BushA2019_statistics[,13:24]),
@@ -71,11 +75,15 @@ master_df = data.frame(S2Bands=S2Bands,
                        ext_BushJ2019_sd=t(ext_BushJ2019_statistics[,13:24]),
                        ext_Clearland2019_sd=t(ext_Clearland2019_statistics[,13:24]),
                        ext_Forest2019_sd=t(ext_Forest2019_statistics[,13:24]),
+                       ext_ForestB2019_sd=t(ext_ForestB2019_statistics[,13:24]),
                        ext_ForestC2019_sd=t(ext_ForestC2019_statistics[,13:24]),
                        ext_Herb2019_sd=t(ext_Herb2019_statistics[,13:24]),
                        ext_HerbsB2019_sd=t(ext_HerbsB2019_statistics[,13:24]),
+                       ext_Ice2019_sd=t(ext_Ice2019_statistics[,13:24]),
                        ext_PeatA2019_sd=t(ext_PeatA2019_statistics[,13:24]),
                        ext_PeatB2019_sd=t(ext_PeatB2019_statistics[,13:24]),
+                       ext_PeatC2019_sd=t(ext_PeatC2019_statistics[,13:24]),
+                       ext_Rocks2019_sd=t(ext_Rocks2019_statistics[,13:24]),
                        ext_Sombra2019_sd=t(ext_Sombra2019_statistics[,13:24]),
                        ext_Waterbodies2019_sd=t(ext_Waterbodies2019_statistics[,13:24]),
                        ext_BushA2019_min=t(ext_BushA2019_statistics[,25:36]),
@@ -90,11 +98,15 @@ master_df = data.frame(S2Bands=S2Bands,
                        ext_BushJ2019_min=t(ext_BushJ2019_statistics[,25:36]),
                        ext_Clearland2019_min=t(ext_Clearland2019_statistics[,25:36]),
                        ext_Forest2019_min=t(ext_Forest2019_statistics[,25:36]),
+                       ext_ForestB2019_min=t(ext_ForestB2019_statistics[,25:36]),
                        ext_ForestC2019_min=t(ext_ForestC2019_statistics[,25:36]),
                        ext_Herb2019_min=t(ext_Herb2019_statistics[,25:36]),
                        ext_HerbsB2019_min=t(ext_HerbsB2019_statistics[,25:36]),
+                       ext_Ice2019_min=t(ext_Ice2019_statistics[,25:36]),
                        ext_PeatA2019_min=t(ext_PeatA2019_statistics[,25:36]),
                        ext_PeatB2019_min=t(ext_PeatB2019_statistics[,25:36]),
+                       ext_PeatC2019_min=t(ext_PeatC2019_statistics[,25:36]),
+                       ext_Rocks2019_min=t(ext_Rocks2019_statistics[,25:36]),
                        ext_Sombra2019_min=t(ext_Sombra2019_statistics[,25:36]),
                        ext_Waterbodies2019_min=t(ext_Waterbodies2019_statistics[,25:36]),
                        ext_BushA2019_max=t(ext_BushA2019_statistics[,37:48]),
@@ -109,16 +121,45 @@ master_df = data.frame(S2Bands=S2Bands,
                        ext_BushJ2019_max=t(ext_BushJ2019_statistics[,37:48]),
                        ext_Clearland2019_max=t(ext_Clearland2019_statistics[,37:48]),
                        ext_Forest2019_max=t(ext_Forest2019_statistics[,37:48]),
+                       ext_ForestB2019_max=t(ext_ForestB2019_statistics[,37:48]),
                        ext_ForestC2019_max=t(ext_ForestC2019_statistics[,37:48]),
                        ext_Herb2019_max=t(ext_Herb2019_statistics[,37:48]),
                        ext_HerbsB2019_max=t(ext_HerbsB2019_statistics[,37:48]),
+                       ext_Ice2019_max=t(ext_Ice2019_statistics[,37:48]),
                        ext_PeatA2019_max=t(ext_PeatA2019_statistics[,37:48]),
                        ext_PeatB2019_max=t(ext_PeatB2019_statistics[,37:48]),
+                       ext_PeatC2019_max=t(ext_PeatC2019_statistics[,37:48]),
+                       ext_Rocks2019_max=t(ext_Rocks2019_statistics[,37:48]),
                        ext_Sombra2019_max=t(ext_Sombra2019_statistics[,37:48]),
                        ext_Waterbodies2019_max=t(ext_Waterbodies2019_statistics[,37:48])
                        )
 
+cover_palette = c("#fffb00", "#6e6d40", "#8a993d",'#e30097','#73d7de','#596104','#c0eb54','#35401a','#d7e6b3','#757219','#a642c7','#00dded','#139ded','#00ffe5','#2fff00','#82ff66','#85ffed','#d4390b','#ff0d00','#731212','#6b6a6a','#000000','#140096')
+
 spectra_plots = ggplot(master_df, aes(S2Bands)) + 
+    geom_ribbon(aes(ymin=ext_Ice2019_mean-ext_Ice2019_sd, ymax=ext_Ice2019_mean+ext_Ice2019_sd),fill=cover_palette[17],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_Rocks2019_mean-ext_Rocks2019_sd, ymax=ext_Rocks2019_mean+ext_Rocks2019_sd),fill=cover_palette[21],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_Waterbodies2019_mean-ext_Waterbodies2019_sd, ymax=ext_Waterbodies2019_mean+ext_Waterbodies2019_sd),fill=cover_palette[23],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_Sombra2019_mean-ext_Sombra2019_sd, ymax=ext_Sombra2019_mean+ext_Sombra2019_sd),fill=cover_palette[22],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_BushA2019_mean-ext_BushA2019_sd, ymax=ext_BushA2019_mean+ext_BushA2019_sd),fill=cover_palette[1],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_BushB2019_mean-ext_BushB2019_sd, ymax=ext_BushB2019_mean+ext_BushB2019_sd),fill=cover_palette[2],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_BushC2019_mean-ext_BushC2019_sd, ymax=ext_BushC2019_mean+ext_BushC2019_sd),fill=cover_palette[3],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_BushD2019_mean-ext_BushD2019_sd, ymax=ext_BushD2019_mean+ext_BushD2019_sd),fill=cover_palette[4],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_BushE2019_mean-ext_BushE2019_sd, ymax=ext_BushE2019_mean+ext_BushE2019_sd),fill=cover_palette[5],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_BushF2019_mean-ext_BushF2019_sd, ymax=ext_BushF2019_mean+ext_BushF2019_sd),fill=cover_palette[6],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_BushG2019_mean-ext_BushG2019_sd, ymax=ext_BushG2019_mean+ext_BushG2019_sd),fill=cover_palette[7],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_BushH2019_mean-ext_BushH2019_sd, ymax=ext_BushH2019_mean+ext_BushH2019_sd),fill=cover_palette[8],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_BushI2019_mean-ext_BushI2019_sd, ymax=ext_BushI2019_mean+ext_BushI2019_sd),fill=cover_palette[9],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_BushJ2019_mean-ext_BushJ2019_sd, ymax=ext_BushJ2019_mean+ext_BushJ2019_sd),fill=cover_palette[10],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_Clearland2019_mean-ext_Clearland2019_sd, ymax=ext_Clearland2019_mean+ext_Clearland2019_sd),fill=cover_palette[11],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_Forest2019_mean-ext_Forest2019_sd, ymax=ext_Forest2019_mean+ext_Forest2019_sd),fill=cover_palette[12],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_ForestB2019_mean-ext_ForestB2019_sd, ymax=ext_ForestB2019_mean+ext_ForestB2019_sd),fill=cover_palette[13],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_ForestC2019_mean-ext_ForestC2019_sd, ymax=ext_ForestC2019_mean+ext_ForestC2019_sd),fill=cover_palette[14],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_Herb2019_mean-ext_Herb2019_sd, ymax=ext_Herb2019_mean+ext_Herb2019_sd),fill=cover_palette[15],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_HerbsB2019_mean-ext_HerbsB2019_sd, ymax=ext_HerbsB2019_mean+ext_HerbsB2019_sd),fill=cover_palette[16],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_PeatA2019_mean-ext_PeatA2019_sd, ymax=ext_PeatA2019_mean+ext_PeatA2019_sd),fill=cover_palette[18],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_PeatB2019_mean-ext_PeatB2019_sd, ymax=ext_PeatB2019_mean+ext_PeatB2019_sd),fill=cover_palette[19],alpha=0.9) +
+    geom_ribbon(aes(ymin=ext_PeatC2019_mean-ext_PeatC2019_sd, ymax=ext_PeatC2019_mean+ext_PeatC2019_sd),fill=cover_palette[20],alpha=0.9) +
     geom_line(aes(y = ext_BushA2019_mean, colour = "BushA")) + 
     geom_line(aes(y = ext_BushB2019_mean, colour = "BushB")) +
     geom_line(aes(y = ext_BushC2019_mean, colour = "BushC")) + 
@@ -135,13 +176,16 @@ spectra_plots = ggplot(master_df, aes(S2Bands)) +
     geom_line(aes(y = ext_ForestC2019_mean, colour = "ForestC")) +
     geom_line(aes(y = ext_Herb2019_mean, colour = "Herb")) + 
     geom_line(aes(y = ext_HerbsB2019_mean, colour = "HerbsB")) +
+    geom_line(aes(y = ext_Ice2019_mean, colour = "Ice")) +
     geom_line(aes(y = ext_PeatA2019_mean, colour = "PeatA")) + 
     geom_line(aes(y = ext_PeatB2019_mean, colour = "PeatB")) +
+    geom_line(aes(y = ext_PeatC2019_mean, colour = "PeatC")) +
+    geom_line(aes(y = ext_Rocks2019_mean, colour = "Rocks")) +
     geom_line(aes(y = ext_Sombra2019_mean, colour = "Sombra")) + 
     geom_line(aes(y = ext_Waterbodies2019_mean, colour = "Waterbodies")) +
-    scale_color_manual(values = c("#fffb00", "#6e6d40", "#8a993d",'#e30097','#73d7de','#596104','#c0eb54','#35401a','#d7e6b3','#757219','#a642c7','#00dded','#139ded','#00ffe5','#2fff00','#82ff66','#d4390b','#ff0d00','#6b6a6a','#140096')) +
-    scale_y_continuous(name='Reflectance [-]') +
-    scale_x_continuous(name='Wavelength [nm]', breaks=master_df$S2Bands, labels=master_df$S2Bands) +
+    scale_color_manual(values = cover_palette) +
+    scale_y_continuous( name='Reflectance [-]') +
+    scale_x_continuous( name='Wavelength [nm]', breaks=master_df$S2Bands, labels=master_df$S2Bands) +
     theme(panel.background = element_rect(fill='white',colour='white'),
       panel.border = element_blank(),
            panel.grid.major = element_blank(),
@@ -152,8 +196,8 @@ spectra_plots = ggplot(master_df, aes(S2Bands)) +
                                         size=15, angle=0))
 
 
-ggsave(paste0(getwd(),'/images/rplots/spectra.png'),spectra_plots,width=500,height=200,units="mm")
-1+1
+ggsave(paste0(getwd(),'/images/rplots/spectra_SD.png'),spectra_plots,width=500,height=200,units="mm")
+
 
 
 
@@ -183,11 +227,6 @@ rownames(df_TD) = spectra_list
 
 
 
-plot1 <- ggplot( aes(v, p)) + 
-  geom_line(x=S2Bands, y=t(get(nameobj_statistics)))# +
-  #geom_step(data = df2)
-#)
-
 for (j in 1:y) {
   for (k in 1:y) {
     # 1. Open jpeg file
@@ -210,14 +249,10 @@ m_M = as.matrix(df_M)
 m_D = as.matrix(df_D)
 m_TD = as.matrix(df_TD)
 
-mm_B= m_B[-20,-20]
-mm_JM= m_JM[-20,-20]
-mm_M= m_M[-20,-20]
-mm_D= m_D[-20,-20]
-mm_TD= m_TD[-20,-20]
-
-corrplot(mm_B, method="circle",is.corr = FALSE)
-corrplot(mm_JM, method="circle",is.corr = FALSE)
-corrplot(mm_M, method="circle",is.corr = FALSE)
-corrplot(mm_D, method="circle",is.corr = FALSE)
-corrplot(mm_TD, method="circle",is.corr = FALSE)
+corrplot_palette = jet.colors(1000)
+color =  c(corrplot_palette,corrplot_palette)
+corrplot(m_B,col=color, method="circle",is.corr = FALSE)
+corrplot(m_JM,col=color, method="circle",is.corr = FALSE)
+corrplot(m_M,col=color, method="circle",is.corr = FALSE)
+corrplot(m_D,col=color, method="circle",is.corr = FALSE)
+corrplot(m_TD,col=color, method="circle",is.corr = FALSE)
